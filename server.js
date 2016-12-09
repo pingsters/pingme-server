a2f78443c94ebdcdd5f0e296a9ac59474951f6e9
Youngchan Je <ochanje210@gmail.com>
2016-12-10 03:02:35 +0900

    init

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('public'));
+
+app.get('/index.htm', function (req, res) {
+  res.sendFile( __dirname + "/" + "index.htm" );
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
